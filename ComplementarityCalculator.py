237dab79914f7d9eea2ef4b1ddad737c4a9ee515
hustnn <nzjemail@gmail.com>
2015-10-02 21:17:00 +0800

    validate of new matrics

--- ComplementarityCalculator.py
+++ ComplementarityCalculator.py
@@ -32,22 +32,111 @@ def dotProduct(v1, v2):
     
 def calConsin(vec1, vec2):
     return float(dotProduct(vec1, vec2)) / (vectorLen(vec1) * vectorLen(vec2))
+
+
+# the smaller, the more complementary
+def calComplementarityOfTwoDVectors(v1, v2):
+    if not isVectorComplementary(v1, v2):
+        return calConsin([1, 1], [1, 1])
+    else:
+        diffV1 = calConsin(v1, [1, 1])
+        diffV2 = calConsin(v2, [1, 1])
+        return max(diffV1, diffV2)
+    
+    
+def genVectorList():
+    return [[0, 10], [1, 9], [2, 8], [3, 7], [4, 6], [5, 5], [6, 4], [7, 3], [8, 2], [9, 1], [10, 0]]
+
+
+def sequentialPacking(vectorList1, vectorList2, clusterCapacity):
+    clusterNum = 0
+    curCap = list(clusterCapacity)
+    for v in vectorList1:
+        if curCap[0] > v[0] and curCap[1] > v[1]:
+            curCap[0] -= v[0]
+            curCap[1] -= v[1]
+        else:
+            clusterNum += 1
+            curCap = list(clusterCapacity)
+            curCap[0] -= v[0]
+            curCap[1] -= v[1]
+            
+    for v in vectorList2:
+        if curCap[0] >= v[0] and curCap[1] >= v[1]:
+            curCap[0] -= v[0]
+            curCap[1] -= v[1]
+        else:
+            clusterNum += 1
+            curCap = list(clusterCapacity)
+            curCap[0] -= v[0]
+            curCap[1] -= v[1]
+    
+    clusterNum += 1
+    return clusterNum
+    
+    
+def efficientPacking(vectorList1, vectorList2, clusterCapacity):
+    clusterNum = 0
+    curCap = list(clusterCapacity)
+    index1 = 0
+    index2 = 0
+    while (index1 < len(vectorList1) and index2 < len(vectorList2)):
+        if  vectorLen(curCap) == 0:
+            clusterNum += 1
+            curCap = list(clusterCapacity) 
+            
+        if calConsin(vectorList1[index1], curCap) > calConsin(vectorList2[index2], curCap):
+            c = vectorList1[index1]
+            index1 += 1
+        else:
+            c = vectorList2[index2]
+            index2 += 1
+        if curCap[0] >= c[0] and curCap[1] >= c[1]:
+            curCap[0] -= c[0]
+            curCap[1] -= c[1]
+        else:
+            clusterNum += 1
+            curCap = list(clusterCapacity)
+            curCap[0] -= c[0]
+            curCap[1] -= c[1]
+    
+    if (index1 < len(vectorList1)):
+        for v in vectorList1[index1::]:
+            if curCap[0] >= v[0] and curCap[1] >= v[1]:
+                curCap[0] -= v[0]
+                curCap[1] -= v[1]
+            else:
+                clusterNum += 1
+                curCap = list(clusterCapacity)
+                curCap[0] -= v[0]
+                curCap[1] -= v[1]
+    else:
+        for v in vectorList2[index2::]:
+            if curCap[0] >= v[0] and curCap[1] >= v[1]:
+                curCap[0] -= v[0]
+                curCap[1] -= v[1]
+            else:
+                clusterNum += 1
+                curCap = list(clusterCapacity)
+                curCap[0] -= v[0]
+                curCap[1] -= v[1]
+                
+    clusterNum += 1
+    return clusterNum
     
 
 if __name__ == '__main__':
-    #print(calConsin([10, 1, 1], [10, 1, 1]))
-    print(calConsin([10, 1, 1], [1, 10, 1]))
-    print(calConsin([10, 1, 10], [1, 10, 1]))
-    #print(calConsin([10, 1, 5], [1, 10, 5]))
-    print(calConsin([10, 10, 1], [1, 10, 1]))
-    
-    print(calConsin([10, 1, 1], [1, 1, 1]))
-    print(calConsin([1, 10, 1], [1, 1, 1]))
-    
-    print(calConsin([10, 1, 10], [1, 1, 1]))
-    print(calConsin([1, 10, 1], [1, 1, 1]))
-    
-    print(calConsin([10, 10, 1], [1, 1, 1]))
-    print(calConsin([1, 10, 1], [1, 1, 1]))
-    
+    clusterCapacity = [100, 100]
+    vectorList = genVectorList()
+    print("vector1\tvector2\tcomplementaryt\timprovement")
+    for vector1 in vectorList:
+        for vector2 in vectorList:
+            # the larger, the better (more complementary)
+            complementarity = '%.2f' % float(1 - calComplementarityOfTwoDVectors(vector1, vector2))
+            vectorList1 = [vector1] * 100
+            vectorList2 = [vector2] * 100
+            seqCost = sequentialPacking(vectorList1, vectorList2, clusterCapacity)
+            efficientCost = efficientPacking(vectorList1, vectorList2, clusterCapacity)
+            improvement = '%.2f' % float(1 - float(efficientCost) / seqCost)
+            print(str(vector1) + "\t" + str(vector2) + "\t" + complementarity + "\t" + improvement)
     
